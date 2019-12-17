cb2ab3e28a6e33b038c03f557819f5f921e30bb8
Giscard Gomperts <giscardgomperts@gmail.com>
2019-12-16 22:47:25 -0400

    SETTING up

---
+++ data/locations.js
@@ -0,0 +1,10 @@
+var places =
+{
+    "type": "FeatureCollection",
+    "name": "locations",
+     "features": [
+    { "type": "Feature", "properties": { "name": "Curacao Buggy Adventures", "description": "Join in on this half-day tour and get ready to discover the most beautiful parts of Curaçao on an ATV also known as a Quad. These top-of-the-line ATV's are suitable for experienced as well as inexperienced drivers. This Eastern Curacao tour provides an off-road adventure, in combination with spectacular views, unique sights and the opportunity to take a dip in the Caribbean sea. It is a unique tour as it quite often takes you off the beaten path with lots of opportunities to take pictures", "type": "buggy", "cost": 153 }, "geometry": { "type": "Point", "coordinates": [ -7670021.696583370678127, 1355990.433568362845108 ] } },
+    { "type": "Feature", "properties": { "name": "Karakter", "description": "Looking for shade, palm trees, clear blue water, great food and drinks and a unique relaxing atmosphere? Stop searching! During daytime Karakter offers a unique beach experience with comfortable lounge chairs, cushions, a lot of shade and the best service. During nighttime Karakter transforms into a cozy sea front restaurant where you can enjoy great dishes for the whole family in a unique atmosphere..", "type": "beach", "cost": 0 }, "geometry": { "type": "Point", "coordinates": [ -7689838.141354219987988, 1368299.989885478513315 ] } }
+    ]
+}
+    
